6b3226a43d3db8710e779abc0504d671770569c2
Naitsab43 <bastianhuenuqueo43@gmail.com>
2022-10-07 15:44:01 -0300

    add changes

--- src/pw-remote/index.js
+++ src/pw-remote/index.js
@@ -1,10 +1,25 @@
+import * as fsPromises from "node:fs/promises";
 import { firefox } from "playwright";
 
 (async () => {
   const browser = await firefox.connect(`ws://localhost:8888`);
   const page = await browser.newPage();
 
-  await page.goto("https://google.com");
-  console.log(await page.title());
+  await page.goto("https://demoqa.com/upload-download");
+
+  // Note that Promise.all prevents a race condition
+  // between clicking and waiting for the download.
+  const [download] = await Promise.all([
+    // It is important to call waitForEvent before click to set up waiting.
+    page.waitForEvent("download"),
+    // Triggers the download.
+    page.locator("#downloadButton").click(),
+  ]);
+  // wait for download to complete
+
+  const aaa = await download.createReadStream();
+
+  await fsPromises.writeFile("./download.jpg", aaa);
+
   await browser.close();
 })();
