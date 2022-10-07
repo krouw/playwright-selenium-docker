4a8ed85af0588e79382901e68f7915d36fb4eb83
Naitsab43 <bastianhuenuqueo43@gmail.com>
2022-10-07 15:11:58 -0300

    add change

--- src/pw-remote/index.js
+++ src/pw-remote/index.js
@@ -1,8 +1,16 @@
 import { firefox } from "playwright";
 
 (async () => {
-  const browser = await firefox.connect(`ws://localhost:4000`);
-  const page = await browser.newPage();
-
-  page.goto("google.com");
+  const browser = await firefox.connect(`ws://localhost:8888/ws`);
+  // const page = await browser.newPage();
+  // await page.goto("https://google.com");
+  // const [download] = await Promise.all([
+  //   // It is important to call waitForEvent before click to set up waiting.
+  //   page.waitForEvent("download"),
+  //   // Triggers the download.
+  //   page.locator("#downloadButton").click(),
+  // ]);
+  // // wait for download to complete
+  // const path = await download.path();
+  // console.log(path);
 })();
