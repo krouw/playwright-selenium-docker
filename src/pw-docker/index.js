8fa34ada833fc30a5abf1bc48c273932149f7a5c
krouw <felipe.andrez@live.cl>
2022-10-07 14:48:50 -0300

    add changes

--- src/pw-docker/index.js
+++ src/pw-docker/index.js
@@ -1,10 +1,10 @@
 import { firefox } from "playwright";
 
 (async () => {
-  const browser = await firefox.launch({
-    headless: false,
-  });
+  const browser = await firefox.launch();
   const page = await browser.newPage();
 
   await page.goto("https://google.com");
+  console.log(await page.title());
+  await browser.close();
 })();
