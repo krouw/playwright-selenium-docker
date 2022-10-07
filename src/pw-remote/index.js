a2f4101483498781e37b4652d157eab11a38c733
krouw <felipe.andrez@live.cl>
2022-10-07 15:32:06 -0300

    add remote

--- src/pw-remote/index.js
+++ src/pw-remote/index.js
@@ -1,16 +1,10 @@
 import { firefox } from "playwright";
 
 (async () => {
-  const browser = await firefox.connect(`ws://localhost:8888/ws`);
-  // const page = await browser.newPage();
-  // await page.goto("https://google.com");
-  // const [download] = await Promise.all([
-  //   // It is important to call waitForEvent before click to set up waiting.
-  //   page.waitForEvent("download"),
-  //   // Triggers the download.
-  //   page.locator("#downloadButton").click(),
-  // ]);
-  // // wait for download to complete
-  // const path = await download.path();
-  // console.log(path);
+  const browser = await firefox.connect(`ws://localhost:8888`);
+  const page = await browser.newPage();
+
+  await page.goto("https://google.com");
+  console.log(await page.title());
+  await browser.close();
 })();
